7c03406eacdb3941d5529bc86369631817eb58dc
Matheus Braga <matheus.braga.work@gmail.com>
2020-11-16 20:51:28 -0300

    :sparkles: Common response to return id

---
+++ src/main/java/br/com/mbragariano/scheduleadministratorapi/common/presentation/models/response/IdResponse.java
@@ -0,0 +1,14 @@
+package br.com.mbragariano.scheduleadministratorapi.common.presentation.models.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Builder
+@AllArgsConstructor
+public class IdResponse {
+
+	@JsonProperty
+	public Long id;
+
+}
