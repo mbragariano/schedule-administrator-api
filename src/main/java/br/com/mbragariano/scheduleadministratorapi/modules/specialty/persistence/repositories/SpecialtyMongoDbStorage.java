86160f5c4c0e2e3e10324fa4cf67fa5d1617867c
Matheus Braga <matheus.braga.work@gmail.com>
2020-11-21 16:11:17 -0300

    :card_file_box: Persistence layer impl for list specialties

--- src/main/java/br/com/mbragariano/scheduleadministratorapi/modules/specialty/persistence/repositories/SpecialtyMongoDbStorage.java
+++ src/main/java/br/com/mbragariano/scheduleadministratorapi/modules/specialty/persistence/repositories/SpecialtyMongoDbStorage.java
@@ -2,17 +2,19 @@ package br.com.mbragariano.scheduleadministratorapi.modules.specialty.persistenc
 
 import br.com.mbragariano.scheduleadministratorapi.common.annotations.Adapter;
 import br.com.mbragariano.scheduleadministratorapi.common.exceptions.DataBaseException;
-import br.com.mbragariano.scheduleadministratorapi.common.persistence.gateways.SpecialtyDocumentGateway;
+import br.com.mbragariano.scheduleadministratorapi.common.persistence.gateways.CreateSpecialtyGateway;
 import br.com.mbragariano.scheduleadministratorapi.common.utils.messageresolver.MessageResolverUtil;
 import br.com.mbragariano.scheduleadministratorapi.modules.specialty.domains.SpecialtyDomain;
 import br.com.mbragariano.scheduleadministratorapi.modules.specialty.persistence.documents.SpecialtyDocument;
 import br.com.mbragariano.scheduleadministratorapi.modules.specialty.ports.SpecialtyStorage;
+import br.com.mbragariano.scheduleadministratorapi.modules.specialty.persistence.gateways.FindAllSpecialtiesGateway;
 import com.mongodb.MongoException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.DataAccessException;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Adapter("specialtyMongoDbStorage")
@@ -25,8 +27,27 @@ public class SpecialtyMongoDbStorage implements SpecialtyStorage {
 	private static final String EXCEPTION_MESSAGE_WHEN_CREATE = "specialty.mongodb.repository.adapter.create.database.exception.message";
 	private static final String EXCEPTION_DETAILS_WHEN_CREATE = "specialty.mongodb.repository.adapter.create.database.exception.details";
 
+	private static final String EXCEPTION_MESSAGE_WHEN_FIND_ALL = "specialty.mongodb.repository.adapter.findAll.database.exception.message";
+	private static final String EXCEPTION_DETAILS_WHEN_FIND_ALL = "specialty.mongodb.repository.adapter.findAll.database.exception.details";
+
 	private static final String EXCEPTION_MESSAGE_WHEN_FIND_BY_NAME = "specialty.mongodb.repository.adapter.existsByName.database.exception.message";
 	private static final String EXCEPTION_DETAILS_WHEN_FIND_BY_NAME = "specialty.mongodb.repository.adapter.existsByName.database.exception.details";
+
+	@Override
+	public List<SpecialtyDomain> findAll() {
+		try {
+			final var specialtyDocuments = this.mongoTemplate.findAll(SpecialtyDocument.class);
+
+			return FindAllSpecialtiesGateway.mapToSpecialtyDomains(specialtyDocuments);
+		} catch (final MongoException mongoException) {
+			mongoException.printStackTrace();
+
+			final var message = this.messageResolverUtil.resolveMessageWithoutParams(EXCEPTION_MESSAGE_WHEN_FIND_ALL);
+			final var details = this.messageResolverUtil.resolveMessageWithoutParams(EXCEPTION_DETAILS_WHEN_FIND_ALL);
+
+			throw new DataBaseException(message, details, mongoException.getMessage(), mongoException.getCode());
+		}
+	}
 
 	@Override
 	public Boolean existsByName(final String name) {
@@ -34,20 +55,20 @@ public class SpecialtyMongoDbStorage implements SpecialtyStorage {
 			final var query = new Query(Criteria.where("name").is(name));
 
 			return this.mongoTemplate.exists(query, SpecialtyDocument.class);
-		} catch (final DataAccessException dataAccessException) {
-			dataAccessException.printStackTrace();
+		} catch (final MongoException mongoException) {
+			mongoException.printStackTrace();
 
 			final var message = this.messageResolverUtil.resolveMessageWithoutParams(EXCEPTION_MESSAGE_WHEN_FIND_BY_NAME);
 			final var details = this.messageResolverUtil.resolveMessageWithoutParams(EXCEPTION_DETAILS_WHEN_FIND_BY_NAME);
 
-			throw new DataBaseException(message, details, dataAccessException.getMessage(), null);
+			throw new DataBaseException(message, details, mongoException.getMessage(), mongoException.getCode());
 		}
 	}
 
 	@Override
 	public void create(final SpecialtyDomain specialtyDomain) {
 		try {
-			final var specialtyDocument = SpecialtyDocumentGateway.mapToSpecialtyDocument(specialtyDomain);
+			final var specialtyDocument = CreateSpecialtyGateway.mapToSpecialtyDocument(specialtyDomain);
 
 			this.mongoTemplate.save(specialtyDocument);
 		} catch (final MongoException mongoException) {
@@ -56,7 +77,7 @@ public class SpecialtyMongoDbStorage implements SpecialtyStorage {
 			final var message = this.messageResolverUtil.resolveMessageWithoutParams(EXCEPTION_MESSAGE_WHEN_CREATE);
 			final var details = this.messageResolverUtil.resolveMessageWithoutParams(EXCEPTION_DETAILS_WHEN_CREATE);
 
-			throw new DataBaseException(message, details, mongoException.getMessage(), null);
+			throw new DataBaseException(message, details, mongoException.getMessage(), mongoException.getCode());
 		}
 	}
 
--- src/main/java/br/com/mbragariano/scheduleadministratorapi/modules/specialty/ports/SpecialtyStorage.java
+++ src/main/java/br/com/mbragariano/scheduleadministratorapi/modules/specialty/ports/SpecialtyStorage.java
@@ -2,7 +2,11 @@ package br.com.mbragariano.scheduleadministratorapi.modules.specialty.ports;
 
 import br.com.mbragariano.scheduleadministratorapi.modules.specialty.domains.SpecialtyDomain;
 
+import java.util.List;
+
 public interface SpecialtyStorage {
+
+	List<SpecialtyDomain> findAll();
 
 	Boolean existsByName(String name);
 
