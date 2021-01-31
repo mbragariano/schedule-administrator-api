f50ce27e2e9be8993ebe2936c9b3a788e863ff63
Matheus Braga <matheus.braga.work@gmail.com>
2021-01-31 01:55:00 -0300

    :memo: Add documentation in endpoints, request and responses

--- src/main/java/br/com/mbragariano/gobeautyapi/modules/specialty/presentation/endpoints/CreateSpecialtyEndPoint.java
+++ src/main/java/br/com/mbragariano/gobeautyapi/modules/specialty/presentation/endpoints/CreateSpecialtyEndPoint.java
@@ -1,9 +1,15 @@
 package br.com.mbragariano.gobeautyapi.modules.specialty.presentation.endpoints;
 
 import br.com.mbragariano.gobeautyapi.common.annotations.RestEndPoint;
+import br.com.mbragariano.gobeautyapi.common.presentation.exceptionhandler.models.responses.BaseExceptionResponse;
+import br.com.mbragariano.gobeautyapi.common.presentation.exceptionhandler.models.responses.EntityValidationExceptionResponse;
 import br.com.mbragariano.gobeautyapi.modules.specialty.facades.CreateSpecialtyFacade;
 import br.com.mbragariano.gobeautyapi.modules.specialty.presentation.models.requests.CreateSpecialtyRequest;
 import br.com.mbragariano.gobeautyapi.modules.specialty.presentation.presenters.CreateSpecialtyPresenter;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
@@ -12,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestEndPoint
+@Api(tags = "Specialty")
 @RequiredArgsConstructor
 public class CreateSpecialtyEndPoint {
 
@@ -20,6 +27,12 @@ public class CreateSpecialtyEndPoint {
 
 	@PostMapping("/specialty")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@ApiOperation(code = 204, value = "Create a specialty")
+	@ApiResponses(value = {
+		@ApiResponse(code = 204, message = "Operation successfully executed"),
+		@ApiResponse(code = 400, message = "Invalid data", response = EntityValidationExceptionResponse.class),
+		@ApiResponse(code = 409, message = "Duplicated Entities", response = BaseExceptionResponse.class),
+	})
 	public void handle(@RequestBody final CreateSpecialtyRequest createSpecialtyRequest) {
 		final var createSpecialtyDto = CreateSpecialtyPresenter.mapToCreateSpecialtyDto(createSpecialtyRequest);
 
